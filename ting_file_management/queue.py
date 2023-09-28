853f36d8982c15c5e9e926651042dc0f115b0d93
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-09-28 18:11:57 -0300

    Implementa lógica para a classe queue nop arquivo queue.py

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,30 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self.queue) == 0:
+            return None
+        return self.queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index > len(self.queue) - 1:
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self.queue[index]
+
+    def __str__(self):
+        return f"""
+            Os arquivos a serem lidos são: {self.queue}
+        """
+
+
+# new_queue = Queue()
+# new_queue.queue(1)
+# print(new_queue)
