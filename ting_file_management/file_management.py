33c8ad91ee97db48e8ccbe367370b7743711534e
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-09-28 19:36:05 -0300

    Implementa a lógica para a função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith(".txt"):
+        try:
+            with open(path_file) as file:
+                return file.read().split("\n")
+        except FileNotFoundError:
+            print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    else:
+        print("Formato inválido", file=sys.stderr)
+
+
+# txt_importer("./statics/arquivo_teste.txt")
+
+# https://www.delftstack.com/pt/howto/python/python-print-to-stderr/
