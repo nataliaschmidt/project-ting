2c863d41a9cd8e542da24ed45135bc55d3439380
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-09-29 14:05:46 -0300

    Implementa a lógica para a função exists_word do arquivo word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,26 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word: str, instance: Queue):
+    output = []
+
+    for file in instance.queue:
+        occurrences = []
+
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": index + 1})
+
+        if occurrences:
+            output.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return output
 
 
 def search_by_word(word, instance):
