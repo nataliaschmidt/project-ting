ce64beb083be26c0bb363390eed17bd013f56272
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-09-29 14:40:30 -0300

    Implementa a lógica para a função search_by_word do arquivo word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -23,5 +23,24 @@ def exists_word(word: str, instance: Queue):
     return output
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def search_by_word(word: str, instance: Queue):
+    output = []
+
+    for file in instance.queue:
+        occurrences = []
+
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": index + 1,
+                                    "conteudo": line})
+
+        if occurrences:
+            output.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return output
