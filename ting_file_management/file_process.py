8994377af5b81e0f421cffb9d14a83defa8baeb3
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-09-28 23:12:34 -0300

    Implementa a lógica para a função process no arquivo file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def process(path_file, instance: Queue):
+    for data in instance.queue:
+        if data["nome_do_arquivo"] == path_file:
+            return None
+
+    new_file = txt_importer(path_file)
+
+    if new_file:
+        file = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(new_file),
+            "linhas_do_arquivo": new_file,
+        }
+
+        instance.enqueue(file)
+        print(file)
 
 
 def remove(instance):
@@ -8,3 +26,14 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+# TESTE = list()
+# TESTE.append(
+#     [
+#         "Acima de tudo,",
+#         "é fundamental ressaltar que a adoção de políticas
+#             descentralizadoras nos obriga",
+#         "à análise do levantamento das variáveis envolvidas.",
+#     ]
+# )
